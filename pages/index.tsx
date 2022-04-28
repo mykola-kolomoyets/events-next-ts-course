e1c7bcf49d26fd5c47239ef88a1b1650baca9fd7
Kolomoyets473 <kolomoyets473@gmail.com>
2022-04-28 19:28:09 +0300

    minor refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,11 @@
 import type { NextPage } from "next";
+import EventsData from "../data";
 
 const Home: NextPage = () => {
+  const featuresEvents = EventsData.getFeaturedEvents();
+
+  console.log(featuresEvents);
+
   return <h1>Home page</h1>;
 };
 
